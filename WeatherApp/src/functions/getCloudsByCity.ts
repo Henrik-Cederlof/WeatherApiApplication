165e314138adb80aa8cf38228441ebba13fba846
Henrik Cederlöf <Henrik.Cederlof@hotmail.com>
2025-01-07 12:58:55 +0100

    Fixade Allt

---
+++ WeatherApp/src/functions/getCloudsByCity.ts
@@ -0,0 +1,7 @@
+import { AllTypes } from '../types/types';
+
+export const getCloudsByCity = async (city: string) => {
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=52f81cddc3fb48a08ff95758250701&q=${city}&`);
+  const data = await response.json() as AllTypes.Root;
+  return data.current.cloud;
+};
--- WeatherApp/src/main.ts
+++ WeatherApp/src/main.ts
@@ -1,6 +1,64 @@
+import { getTemeratureBYCity } from './functions/getTemeratureBYCity';
+import { getCloudsByCity } from './functions/getCloudsByCity';
+import { getWeatherCondition } from './functions/getWeatherCondition';
+
 import './style.scss';
 
 
+const searchInput = document.getElementById('search-input') as HTMLInputElement;
+const searchButton = document.getElementById('search-button') as HTMLButtonElement;
+const temperatureDiaplay = document.getElementById('temperature-display') as HTMLDivElement;
+const mainContainer = document.getElementById('main') as HTMLDivElement;
+
+const createWeatherCard = (cityName: string, temperature: number, condition: string) => {
+  const card = document.createElement('div');
+  card.className = 'weather-card';
+
+  const leftSection = document.createElement('div');
+  leftSection.className = 'left-section';
+
+  const cityElement = document.createElement('h2');
+  cityElement.className = 'city-name';
+  cityElement.textContent = cityName;
+  
+  const temeratureElement = document.createElement('div');
+  temeratureElement.className = 'temperature';
+  temeratureElement.textContent = `${temperature}°C`;
+
+  const conditionElement = document.createElement('img');
+  conditionElement.className = 'condition-icon';
+  conditionElement.src = condition;
+  conditionElement.alt = 'Weather condition';
+
+  leftSection.appendChild(cityElement);
+  leftSection.appendChild(conditionElement);
 
 
 
+  // Lägg till i kortet
+  card.appendChild(leftSection);
+  card.appendChild(temeratureElement);
+
+  return card;
+}
+
+searchButton.addEventListener('click', async (event) => {
+  event.preventDefault();
+  if(searchInput.value) {
+    try {
+      const cityName = searchInput.value;
+      const temperature = await getTemeratureBYCity(cityName);
+      const condition = await getWeatherCondition(cityName);
+
+      const weaterCard = createWeatherCard(cityName, temperature, condition);
+      mainContainer.appendChild(weaterCard);
+
+      searchInput.value = '';
+    }
+    catch(error) {
+      console.error(error);
+      temperatureDiaplay.textContent = 'City not found';
+    }
+  }
+  
+});
