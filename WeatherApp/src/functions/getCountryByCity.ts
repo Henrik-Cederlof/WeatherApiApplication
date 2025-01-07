9277b6eb0a10d746304eca15a85ca1bf0368a7ef
HanninenF <hanninenfredrik@gmail.com>
2025-01-07 16:55:15 +0100

    abu dhabi

--- WeatherApp/src/functions/getCountryByCity.ts
+++ WeatherApp/src/functions/getCountryByCity.ts
@@ -5,5 +5,19 @@ export const getCountryByCity = async (city: string) => {
     `https://api.weatherapi.com/v1/current.json?key=52f81cddc3fb48a08ff95758250701&q=${city}&`
   );
   const data = (await response.json()) as AllTypes.Root;
-  return data.location.country;
+  const country: string = data.location.country;
+
+  const countryArray: string[] = data.location.country.split(" ");
+  console.log("countryArray=", countryArray);
+
+  if (countryArray.length > 1) {
+    const abbrevation: string = country
+      .split(" ")
+      .map((word) => word.charAt(0))
+      .filter((char) => char === char.toUpperCase() && char.match(/[A-Z]/))
+      .join("");
+
+    return abbrevation;
+  }
+  return countryArray[0];
 };
